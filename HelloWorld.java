4ccb5be7082712705892ff1922952c7f3a2347c4
willgray92 <66655383+willgray92@users.noreply.github.com>
2020-08-24 21:22:40 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,8 +6,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class c1 = new Class1(Class1);
+        Class c2 = new Class2(Class2);
         System.out.println("Hello, World!");
     }
     private void f1() {
